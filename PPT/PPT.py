34952d8e3ff6c6f0b6bc956c777f7959a42a43e5
breixo14 <breixodocampo@gmail.com>
2024-12-28 17:45:04 +0100

    Reestructuración

---
+++ PPT/PPT.py
@@ -0,0 +1,21 @@
+import random
+
+rock="piedra"
+paper="papel"
+scissors="tijeras"
+
+opciones = [rock, paper, scissors]
+
+def Eleccion_maquina():
+    print(random.choice(opciones))
+
+def Eleccion_jugador():
+    jugador_eleccion=input("R/P/T")
+    if   jugador_eleccion=="R":
+        jugador_eleccion=rock
+    if   jugador_eleccion=="P":
+        jugador_eleccion=paper
+    else :
+        jugador_eleccion=scissors
+    print(jugador_eleccion)
+
